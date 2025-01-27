91c4fb2f357137917ac7ffb68993e4ce00f6f825
Chaitanya Kumar Dewangan <chaitanyakumar1401@gmail.com>
2025-01-27 22:10:41 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const url =
+    'https://img.freepik.com/premium-vector/background-green-grass-with-butterfly_100121-132.jpg?w=740';
+const description =
+    '''A serene oasis in my pocket, mirroring the beauty of sunlit forests and calming waves. Its screen is a canvas for sunsets, stars, and the dance of leaves. With every touch, I feel connected to nature's rhythm. A window to explore the Earth's wonders from anywhere.''';
+
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
@@ -7,13 +12,113 @@ void main() {
         backgroundColor: Colors.green,
         centerTitle: true,
         //by default it is in left
-        title: Text('Home Page'),
+        title: Text('Nature'),
       ),
-      body: Center(
-        child: Text(
-          'Hello world', style: TextStyle(fontSize: 23.0),
-          textDirection: TextDirection.ltr,
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.network(
+            url,
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.contain,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Oeschinen Lake Campground',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('Kandersteg SwitezerLand'),
+                        ),
+                      ]),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 18.0 ,right: 8.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                              Icons.star,
+                            color: Colors.orange,
+                            ),
+
+                        Text('41'),
+                      ],
+                    ),
+                  ),
+
+                ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(
+                      Icons.call,
+                      color: Colors.blue,
+                      size: 18,
+                    ),
+                    Text(
+                      'CALL',
+                      style: TextStyle(color: Colors.blue, fontSize: 14),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.send,
+                      color: Colors.blue,
+                      size: 18,
+                    ),
+                    Text(
+                      'ROUTE',
+                      style: TextStyle(color: Colors.blue, fontSize: 14),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.share,
+                      color: Colors.blue,
+                      size: 18,
+                    ),
+                    Text(
+                      'SHARE',
+                      style: TextStyle(color: Colors.blue, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              description,
+              textAlign: TextAlign.justify,
+            ),
+          ),
+        ],
       ),
     ),
   ));
